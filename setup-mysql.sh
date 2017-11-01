e9c2d2c5955694301b880c2f7be0fbc9f6aaf1dd
deadsik <deadsik@ukr.net>
2017-11-01 16:41:43 +0200

    Update setup-mysql.sh

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,5 +1,3 @@
-# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
-echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
-apt-get -y install mysql-server
-mysql_secure_installation
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
+sudo apt-get install -y mysql-server
