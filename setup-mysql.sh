2bfaeb17662cf253971bcf9a85c5b1d5279b84ba
deadsik <deadsik@ukr.net>
2017-11-01 17:23:47 +0200

    Update setup-mysql.sh

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >> /etc/apt/sources.list;
-echo "deb-src http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >>  /etc/apt/sources.list;
-apt-get -y update;
-yes | apt-get install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
-apt-get -y --force-yes install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6 -y --force-yes;
+VAR=$(expect -c '
+spawn apt-get -y install mysql-server
+expect "New password for the MySQL \"root\" user:"
+send "PasswordHere\r"
+expect "Repeat password for the MySQL \"root\" user:"
+send "PasswordHere\r"
+expect eof
+')
+echo "$VAR"
