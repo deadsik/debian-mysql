f2b59302f4328ec99444ce9f6624c642c18f8d6c
deadsik <deadsik@ukr.net>
2017-11-01 17:07:46 +0200

    Update setup-mysql.sh

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,3 +1,7 @@
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
-sudo apt-get install -y mysql-server
+#!/bin/bash
+echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >> /etc/apt/sources.list;
+echo "deb-src http://repo.mysql.com/apt/debian/ jessie mysql-5.6" >>  /etc/apt/sources.list;
+apt-get -y update;
+yes | apt-get install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
+apt-get -y --force-yes install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6;
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install mysql-server-5.6 mysql-client-5.6 mysql-client-core-5.6 -y --force-yes;
